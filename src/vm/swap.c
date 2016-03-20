ab8981448df7111007f55c44e87f1f2b88ec20b1
Daniel Budiono <db3014@matrix12.doc.ic.ac.uk>
2016-03-20 16:25:44 +0000

    Add implementation for writing file back in munmap

---
+++ src/vm/swap.c
@@ -0,0 +1,5 @@
+#include "vm/swap.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+
---
+++ src/vm/swap.h
@@ -0,0 +1,10 @@
+#ifndef SWAP_H_
+#define SWAP_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+
+
+
+#endif /* SWAP_H_ */
