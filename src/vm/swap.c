e5fb55bfba8a17d64dcc89e164d2a370d7012494
bp2214 <bp2214@ic.ac.uk>
2016-03-21 17:54:23 +0000

    swap comments

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -11,9 +11,13 @@
 
 #define SECTORS_IN_PAGE 8
 
+/* Lock for synchronising swaps */
 static struct lock swap_lock;
+/* The block that takes care of the actual swapping */
 static struct block *swap_block;
+/* A Bitmap to store the position of swapped pages in the swap partition */
 static struct bitmap *sector_bm;
+/* A hashmap for the actual swap table */
 static struct hash swap_table;
 
 static unsigned swap_hash (const struct hash_elem *e, void *aux UNUSED);
@@ -21,12 +25,14 @@ static bool swap_less (const struct hash_elem *a, const struct hash_elem *b,
                        void *aux UNUSED);
 static struct hash_elem *swap_lookup (void *uaddr);
 
+/* The swap-table elements contain the virtual user address and the sector they are in */
 struct swap {
   struct hash_elem swaphashelem;
   void *uaddr;
   uint32_t sector;
 };
 
+/* Initialises the global static variables */
 void
 swap_init (void)
 {
@@ -38,6 +44,7 @@ swap_init (void)
   hash_init (&swap_table, swap_hash, swap_less, NULL);
 }
 
+/* Swaps a page from the swap partition back into memory */
 bool
 swap_in(void *page_addr)
 {
@@ -45,9 +52,11 @@ swap_in(void *page_addr)
   // if doesn't exist, return failure of loading in
   if (bm_sector == -1)
     return false;
+  // Set up a buffer to read page into
   void* buffer = malloc(BLOCK_SECTOR_SIZE);
   if (buffer == NULL)
     return false;
+  // Read all SECTORS_IN_PAGE sectors from the swap partition into memory
   int i;
   for (i = 0; i < SECTORS_IN_PAGE; i++)
     {
@@ -59,37 +68,47 @@ swap_in(void *page_addr)
   return true;
 }
 
+/* Removes page from swap_table and updates bitmap */
 int64_t
 swap_free(void *page_addr)
 {
+	// Acquire the swap table element to delete
   lock_acquire (&swap_lock);
   struct swap *s = hash_entry(hash_find(&swap_table, swap_lookup(page_addr)), struct swap, swaphashelem);
   lock_release (&swap_lock);
 
+  // If element is found:
   if (s != NULL)
       {
+	  // get the sector of the swap partition where the page is stored
+      size_t bm_sector = s->sector;
+	  // Delete the swap_table element
         lock_acquire (&swap_lock);
         hash_delete(&swap_table, &s->swaphashelem);
         lock_release (&swap_lock);
 
-        size_t bm_sector = s->sector;
+        // free the element
         free(s);
+// update the bitmap
         bitmap_scan_and_flip(sector_bm, bm_sector, SECTORS_IN_PAGE, true);
-        //On finding and successful deletion return bit map index
+        //return the sector where the page is stored
         return bm_sector;
       }
-  //if doesn't exist return invalid index -1
+  // If element is not found, return invalid index -1
   return -1;
 }
 
+/* Swaps a page from memory into the swap partition */
 bool
 swap_out(uint32_t *pd, void *page_addr)
 {
+	// If the page is clean, it doesn't have to be copied, so just return true
   if(!pagedir_is_dirty(pd, page_addr))
     return true;
 
-  // mark swap table entry in bitmap
+  // mark swap_table entry in bitmap
   size_t bm_sector = bitmap_scan_and_flip(sector_bm, 0, SECTORS_IN_PAGE, false);
+  // Panic the kernel if there is no space on the partition
   if(bm_sector == BITMAP_ERROR)
     PANIC("ERROR: Swap partition full!");
   //create swap_table entry
@@ -98,10 +117,11 @@ swap_out(uint32_t *pd, void *page_addr)
     PANIC ("swap_multiple: out of memory");
   s->uaddr = page_addr;
   s->sector = bm_sector;
+  // Insert the entry into the swap_table
   lock_acquire (&swap_lock);
   hash_insert (&swap_table, &s->swaphashelem);
   lock_release (&swap_lock);
-  // Copy frame = 8 sectors
+  // Copy page from frame into SECTORS_IN_PAGE sectors in the swap partition
   void* buffer = malloc(BLOCK_SECTOR_SIZE);
   if (buffer == NULL)
     PANIC ("swap_multiple: out of memory");
@@ -115,6 +135,7 @@ swap_out(uint32_t *pd, void *page_addr)
   return true;
 }
 
+/* Hash helper for the swap_table hash_table */
 static unsigned
 swap_hash (const struct hash_elem *e, void *aux UNUSED)
 {
@@ -122,6 +143,7 @@ swap_hash (const struct hash_elem *e, void *aux UNUSED)
   return hash_bytes (&s->uaddr, sizeof s->uaddr);
 }
 
+/* Hash helper for the swap_table hash_table */
 static bool
 swap_less (const struct hash_elem *a, const struct hash_elem *b,
            void *aux UNUSED)
@@ -130,6 +152,7 @@ swap_less (const struct hash_elem *a, const struct hash_elem *b,
       hash_entry (b, struct swap, swaphashelem)->uaddr;
 }
 
+/* Returns the hash_elem corresponding to the given virtual user address */
 static struct hash_elem *
 swap_lookup (void *uaddr)
 {
